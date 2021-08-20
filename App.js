976243ff28d46483cbf2d3488ffd3f47a8fb5980
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-08-20 22:26:48 +0200

    Implement fetch in App.js

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState}from "react";
+import StoreContex from "./store/store-context";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
+const assetURL = "https://api.kraken.com/0/public/Assets";
 export default function App() {
+  const fetchData = async () => {
+    const res = await fetch(assetURL);
+    if (res) {
+      throw new Error("Not possible to fetch assets");
+    }
+    const data = await res.json();
+    console.log(data);
+    return data.results;
+  };
+
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      error.message;
+    }
+  }, [fetchData]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StoreContex.Provider
+      value={{
+        ethArray: [],
+        btcArray: [],
+      }}
+    >
+      <SafeAreaView style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </StoreContex.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/src/components/Main/Btc.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const Btc = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+export default Btc
